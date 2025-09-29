e3b012530677e26f1484a9d03d48586c79e65c19
motanafi <twaha2030@gmail.com>
2025-09-29 13:16:19 +0300

    get post in php

--- index.php
+++ index.php
@@ -9,6 +9,22 @@ is
 a multiline comment 
 */
 
+// $_GET, $_POST = special variables used to collect data from an 
+//         HTML data is sent to the file in the action attribute of <form?
+//         <form action="some_file.php" method="get/post"> 
+
+//$_GET = Data is appended to the url 
+//        NOT SECURE 
+//        char limit 
+//         Bookmark is possible w/ values
+//         Get request can be cached
+//         Better for a search page 
+//$_POST = Dataa is packaged inside the body of the http request
+//   MORE SECURE 
+//   NO DATA LIMIT 
+// CAN NOT BOOKMARK
+// GET request is not cached
+// better for submiting credential data
  ?>
  <!DOCTYPE html>
  <html lang="en">
