e0a8d3588781f80c6299d2a2f4ff90843107875d
motanafi <twaha2030@gmail.com>
2025-10-02 13:09:11 +0300

    logical operators in php

--- index.php
+++ index.php
@@ -29,19 +29,29 @@
 //     echo "You are not eligible to vote.";
 //  }
 
-$adult = false;
-$hours = 40;
-$rate = 15;
-$weekly_pay= null;
+// $adult = false;
+// $hours = 40;
+// $rate = 15;
+// $weekly_pay= null;
 
-if($hours <= 40){
-    $weekly_pay = $hours * $rate;
-    echo "Your weekly pay is: $" . $weekly_pay;
-  } elseif($hours > 40 && $adult == true){
-    $overtime_hours = $hours - 40;
-    $weekly_pay = (40 * $rate) + ($overtime_hours * ($rate * 1.5));
-    echo "Your weekly pay is: $" . $weekly_pay;
-  } else {
-    echo "You are not eligible for overtime pay.";
+// if($hours <= 40){
+//     $weekly_pay = $hours * $rate;
+//     echo "Your weekly pay is: $" . $weekly_pay;
+//   } elseif($hours > 40 && $adult == true){
+//     $overtime_hours = $hours - 40;
+//     $weekly_pay = (40 * $rate) + ($overtime_hours * ($rate * 1.5));
+//     echo "Your weekly pay is: $" . $weekly_pay;
+//   } else {
+//     echo "You are not eligible for overtime pay.";
+// }
+
+// LOGICAL OPERATORS
+$age = 25;
+$citizen = false;
+
+if($age >= 18 && $citizen ){
+  echo"You are eligible to vote.";
+} else {
+  echo"You are not eligible to vote.";
 }
  ?>
